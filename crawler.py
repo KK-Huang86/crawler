0e368acf54bc9d2f7b0bf11631d523c9ed6870f6
Kk <iankk0806@gmail.com>
2024-10-20 23:40:38 +0800

    update README

--- crawler.py
+++ crawler.py
@@ -1,24 +1,29 @@
 import requests
 from bs4 import BeautifulSoup
+import json
 
 url="https://www.ptt.cc/bbs/Japan_Travel/index.html"
 headers={'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.36'}
 response=requests.get(url,headers=headers)
 soup=BeautifulSoup(response.text,"html.parser")
 article=soup.find_all("div", class_="r-ent")
+data_list=[]
 for x in article:
+  data={}
   title=x.find("div",class_="title")
   if title and title.a:
     title=title.a.text
   else:
     title="沒有標題" 
-
+  data["標題"]=title
 
   popular=x.find("div",class_="nrec")
   if popular and popular.span:
     popular=popular.span.text
   else:
     popular="無法顯示"
+  data["人氣"]=popular
+
 
   date=x.find("div",class_="date")
   if date:
@@ -26,4 +31,10 @@ for x in article:
   else:
     date="沒有日期"
 
-  print(f"標題:{title} 人氣:{popular} 日期:{date}")
+  data["日期"]=date
+  
+  data_list.append(data)
+
+print(data_list)
+
+  # print(f"標題:{title} 人氣:{popular} 日期:{date}")
