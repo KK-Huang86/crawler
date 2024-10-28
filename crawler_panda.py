61aa1fa99aba957cdef528fe65a0800eea5cf814
Kk <iankk0806@gmail.com>
2024-10-28 16:36:23 +0800

    data_list transfer json & execl

--- crawler_panda.py
+++ crawler_panda.py
@@ -1,6 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
-import json
+import pandas
 
 url="https://www.ptt.cc/bbs/Japan_Travel/index.html"
 headers={'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.36'}
@@ -35,10 +35,8 @@ for x in article:
   
   data_list.append(data) #將抓回來的資料寫進data_list
 
-with open ("ptt_japan_data.json","w",encoding="utf-8") as file:
-  json.dump(data_list,file,ensure_ascii=False,indent=4)
+result=pandas.DataFrame(data_list)
+result.to_excel("PTT_Japan_Travel.xls",index=False,engine="openpyxl") #將result轉成excel,第一個空格為檔案名稱，以及開啟的方式
 
 print("資料已經成功轉換")
 print(data_list)
-
-  # print(f"標題:{title} 人氣:{popular} 日期:{date}")
